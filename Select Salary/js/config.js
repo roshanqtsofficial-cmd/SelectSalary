ac1a8d4d52d33199ced0d74d3d38e5ab1db2c668
roshanqtsofficial-cmd <roshan.qtsofficial@gmail.com>
2026-09-11 20:56:33 +0530

    Delete Select Salary directory

--- Select Salary/js/config.js
+++
@@ -1,4 +0,0 @@
-/* Shared backend endpoint for the public site and admin portal. */
-
-const SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbzGV765IlokNMc_FsPgx7IIul_jkfSutpWu78Lt-ZUu9IYy3US8ZYkBAU61GqTsseaWvQ/exec";
